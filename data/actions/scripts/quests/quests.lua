141afb64822e1fb7323adc76f9fb0e069669d785
gianpeiter <giancpeiter@gmail.com>
2026-05-07 11:59:22 +0000

    improvements to variable names #2

--- data/actions/scripts/quests/quests.lua
+++ data/actions/scripts/quests/quests.lua
@@ -1,29 +1,138 @@
+local config = {
+	-- Battle Axe Quest
+    [3000] = {
+        items = {
+            {itemId = 2378, count = 1}
+        },
+        storage = 5673,
+		emptyMessage = "The pile of bones is empty."
+    }
+}
+
+local function getRewardText(itemId, count)
+    local item = getItemInfo(itemId)
+
+    count = count or 1
+
+    if count > 1 then
+        return count .. " " .. item.plural
+    end
+
+    if item.article ~= "" then
+        return item.article .. " " .. item.name
+    end
+
+    return item.name
+end
+
 function onUse(cid, item, fromPosition, itemEx, toPosition)
-	if(getBooleanFromString(getConfigValue('enableProtectionQuestForGM'))) then
-		if(getPlayerCustomFlagValue(cid, PLAYERCUSTOMFLAG_GAMEMASTERPRIVILEGES)) then
-			doSendMagicEffect(getCreaturePosition(cid), CONST_ME_POFF, cid)
+    local useItem = config[item.uid]
+
+    if not useItem then
+        return true
+    end
+
+    local storage = getPlayerStorageValue(cid, useItem.storage)
+
+	if useItem.time then
+		if storage > os.time() then
+			doPlayerSendCancel(cid, useItem.emptyMessage or ("The " .. getItemNameById(item.itemid) .. " is empty."))
+			return true
+		end
+	else
+		if storage ~= (useItem.formerValue or -1) then
+			doPlayerSendCancel(cid, useItem.emptyMessage or ("The " .. getItemNameById(item.itemid) .. " is empty."))
 			return true
 		end
 	end
 
-	if item.uid > 1000 and item.uid < 12660 then
-		local itemWeight = getItemWeightById(item.uid, 1)
-		local playerCap = getPlayerFreeCap(cid)
-		if getPlayerStorageValue(cid, item.uid) == -1 then
-			if playerCap >= itemWeight then
-				doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, 'You have found a ' .. getItemNameById(item.uid) .. '.')
-				doPlayerAddItem(cid, item.uid ,1)
-				setPlayerStorageValue(cid, item.uid, 1)
-				doPlayerSaveEx(cid)
-			else
-				doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, 'You have found a ' .. getItemNameById(item.uid) .. ' weighing ' .. itemWeight .. ' oz it\'s too heavy.')
-			end
-		else
-			doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "It is empty.")
-		end
-	else
-		return false
-	end
+    if useItem.needItem then
+        if getPlayerItemCount(cid, useItem.needItem.itemId) < (useItem.needItem.count or 1) then
+            doPlayerSendCancel(cid, "You do not have the required item.")
+            return true
+        end
+    end
 
-	return true
+    local items = useItem.items
+    local result = ""
+
+    if #items == 1 then
+        local reward = doCreateItemEx(items[1].itemId, items[1].count or 1)
+
+        if items[1].actionId then
+            doItemSetAttribute(reward, "aid", items[1].actionId)
+        end
+
+        if items[1].text then
+            doItemSetAttribute(reward, "text", items[1].text)
+        end
+
+        if doPlayerAddItemEx(cid, reward, true) ~= RETURNVALUE_NOERROR then
+            doPlayerSendCancel(cid, "You do not have enough capacity or space.")
+            return true
+        end
+
+        if items[1].decay then
+            doDecayItem(reward)
+        end
+
+        result = getRewardText(items[1].itemId, items[1].count)
+    else
+        local backpackId = (#items > 8) and 1988 or 1987
+
+        local container = doCreateItemEx(backpackId, 1)
+
+        for _, rewardData in ipairs(items) do
+            local tmp = doCreateItemEx(rewardData.itemId, rewardData.count or 1)
+
+            if rewardData.actionId then
+                doItemSetAttribute(tmp, "aid", rewardData.actionId)
+            end
+
+            if rewardData.text then
+                doItemSetAttribute(tmp, "text", rewardData.text)
+            end
+
+            doAddContainerItemEx(container, tmp)
+
+            if rewardData.decay then
+                doDecayItem(tmp)
+            end
+        end
+
+        if doPlayerAddItemEx(cid, container, true) ~= RETURNVALUE_NOERROR then
+            doPlayerSendCancel(cid, "You do not have enough capacity or space.")
+            return true
+        end
+
+        local bpInfo = getItemInfo(backpackId)
+
+        result = bpInfo.article .. " " .. bpInfo.name
+    end
+
+    if useItem.say then
+        doCreatureSay(cid, useItem.say, TALKTYPE_ORANGE_1)
+    end
+
+    if useItem.needItem then
+        doPlayerRemoveItem(cid, useItem.needItem.itemId, useItem.needItem.count or 1)
+    end
+
+    if useItem.effect then
+        doSendMagicEffect(toPosition, useItem.effect)
+    end
+
+    if useItem.missionStorage then
+        setPlayerStorageValue(cid, useItem.missionStorage.key, useItem.missionStorage.value)
+    end
+
+    doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You have found " .. result .. ".")
+
+    if useItem.time then
+        setPlayerStorageValue(cid, useItem.storage, os.time() + 86400)
+    else
+        setPlayerStorageValue(cid, useItem.storage, useItem.newValue or 1)
+    end
+
+    return true
 end
